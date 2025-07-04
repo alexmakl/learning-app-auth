abae10feba9c26915ba370f36d92668b0f2fdb7d
Alexander <mclakov@yandex.ru>
2025-07-04 10:35:00 +0300

    project created

---
+++ LearningCards/LearningCardsApp.swift
@@ -0,0 +1,20 @@
+//
+//  LearningCardsApp.swift
+//  LearningCards
+//
+//  Created by Alexander on 04.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct LearningCardsApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
